737d9672ed6652e786befc46f2876e51e9d03dee
IdoLev95 <idolev0611@gmail.com>
2022-04-13 16:25:34 +0000

    run the code via google colab

--- Training/TrainModel.py
+++ Training/TrainModel.py
@@ -9,6 +9,7 @@ import torch.utils.data
 #from Dataset.DataSet import ShapeNetDataset
 #from Models import PointNetCls, feature_transform_regularizer
 import torch.nn.functional as F
+from torch.utils.data import dataloader
 from tqdm import tqdm
 # importing sys
 import sys
@@ -29,7 +30,7 @@ parser.add_argument(
 parser.add_argument(
     '--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument(
-    '--nepoch', type=int, default=250, help='number of epochs to train for')
+    '--nepoch', type=int, default=1, help='number of epochs to train for')
 parser.add_argument('--outf', type=str, default='cls', help='output folder')
 parser.add_argument('--model', type=str, default='', help='model path')
 #parser.add_argument('--dataset', type=str, required=True, help="dataset path")
@@ -38,7 +39,7 @@ parser.add_argument('--feature_transform', action='store_true', help="use featur
 
 opt = parser.parse_args()
 print(opt)
-
+blue = lambda x: '\033[94m' + x + '\033[0m'
 if opt.dataset_type == 'shapenet':
     opt.dataset = '/content/drive/MyDrive/shapenetcore_partanno_segmentation_benchmark_v0'
     dataset = ShapeNetDataset(
@@ -93,38 +94,47 @@ scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 classifier.cuda()
 
 
-um_batch = len(dataset) / opt.batchSize
+num_batch = len(dataset) / opt.batchSize
 
 for epoch in range(opt.nepoch):
-    scheduler.step()
-    for i, data in enumerate(dataloader, 0):
-        points, target = data
-        target = target[:, 0]
-        points = points.transpose(2, 1)
-        points, target = points.cuda(), target.cuda()
-        optimizer.zero_grad()
-        classifier = classifier.train()
-        pred, trans, trans_feat = classifier(points)
-        loss = F.nll_loss(pred, target)
-        if opt.feature_transform:
-            loss += feature_transform_regularizer(trans_feat) * 0.001
-        loss.backward()
-        optimizer.step()
-        pred_choice = pred.data.max(1)[1]
-        correct = pred_choice.eq(target.data).cpu().sum()
-        print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item() / float(opt.batchSize)))
+   with tqdm(dataloader, unit="batch") as tepoch:
+      lossOfEpoch = []
+      accOfEpoch = []
+      
+      scheduler.step()
+      for batchInd,data in enumerate(tepoch,0):
+          tepoch.set_description(f"Epoch {epoch}")
+          points, target = data
+          target = target[:, 0]
+          points = points.transpose(2, 1)
+          points, target = points.cuda(), target.cuda()
+          optimizer.zero_grad()
+          classifier = classifier.train()
+          pred, trans, trans_feat = classifier(points)
+          loss = F.nll_loss(pred, target)
+          
+          if opt.feature_transform:
+              loss += feature_transform_regularizer(trans_feat) * 0.001
+          loss.backward()
+          optimizer.step()
+          pred_choice = pred.data.max(1)[1]
+          correct = pred_choice.eq(target.data).cpu().sum()
+          lossOfEpoch.append(loss.item())
+          accOfEpoch.append(correct.item() / float(opt.batchSize))
+          #print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item() / float(opt.batchSize)))
+          tepoch.set_postfix(loss=sum(lossOfEpoch)/len(lossOfEpoch), accuracy=100. * sum(accOfEpoch)/len(accOfEpoch))
+          if 1 % 10 == 0:
+              j, data = next(enumerate(testdataloader, 0))
+              points, target = data
+              target = target[:, 0]
+              points = points.transpose(2, 1)
+              points, target = points.cuda(), target.cuda()
+              classifier = classifier.eval()
+              pred, _, _ = classifier(points)
+              loss = F.nll_loss(pred, target)
+              pred_choice = pred.data.max(1)[1]
+              correct = pred_choice.eq(target.data).cpu().sum()
+           #   print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
+          
 
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
-            points, target = data
-            target = target[:, 0]
-            points = points.transpose(2, 1)
-            points, target = points.cuda(), target.cuda()
-            classifier = classifier.eval()
-            pred, _, _ = classifier(points)
-            loss = F.nll_loss(pred, target)
-            pred_choice = pred.data.max(1)[1]
-            correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
-
-    torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
+   torch.save(classifier.state_dict(), '/content/UnsupervisedSaliencyPointCloud/cls/cls_model_%d.pth' % (epoch))
