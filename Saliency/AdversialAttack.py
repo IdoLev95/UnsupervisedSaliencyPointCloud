66a73e0a073d940b34432f20f057ed016145c410
IdoLev95 <idolev0611@gmail.com>
2022-05-06 07:06:46 +0000

    Adversial grad cam

--- Saliency/AdversialAttack.py
+++ Saliency/AdversialAttack.py
@@ -1,6 +1,6 @@
 import argparse
 import numpy as np
-import pytorch
+import torch
 import socket
 import importlib
 import copy
@@ -12,16 +12,38 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 class AdversialPointCloud():
     def __init__(self, BATCH_SIZE, NUM_CLASSES,Th):
         self.Th = Th
-        self.BATCH_SIZE = BATCH_SIZE
-        self.NUM_CLASSES = NUM_CLASSES
     def GetSaliencyGradCam(self, model,pointCloud,label):
-        pred = np.argmax(model.forward(pointCloud))
-        pointCloudCopy = pointCloud.Copy()
+        with tqdm(dataloader, unit="batch") as tepoch:
+      
+          for batchInd,data in enumerate(tepoch,0):
+              if(batchInd> 20):
+                continue
+              batchSize = points.shape[0]  
+              points, target = data
+              target = target[:, 0]
+              points = points.transpose(2, 1)
+              points, target = points.cuda(), target.cuda()
 
-        while pred == label:
+              classifier.feat.activateBackwrdHook()
+              for indBatch in range(batchSize):
+                pointCloud = points[indBatch,:].unsqueeze(0)
+                singleTarget = target.unsqueeze(0)
+                pred, trans, trans_feat = classifier(pointCloud)
+                
+                loss = F.nll_loss(pred, singleTarget)
+                
+
+                loss.backward()
+
+                gradientsPerPoint =torch.max(classifier.feat.gradients[0][0], 1, keepdim=False)[0]
+                #### getA is the sum of features where gradientsPerPoint are the re the gradients according to the last layer
+                getA = classifier(points, True)
+
+        """while pred == label:
           derivatives = model.GetDevs(pointCloudCopy)
           weights = model.GetWeights(pointCloudCopy,derivatives)
           pointCloudCopy,placesWhereRemovedAndScores = removeNeededPoints(pointCloudCopy,weights,self.Th)
           pred = np.argmax(model.forward(pointCloud))
         return dictPlacesAndScores
+        """
         
