f289af4c7a09ba3b9bf993be2b6f0e3e3752572a
IdoLev95 <idolev0611@gmail.com>
2022-05-06 07:19:57 +0000

    Adversial grad cam

--- Saliency/AdversialAttack.py
+++ Saliency/AdversialAttack.py
@@ -6,18 +6,19 @@ import importlib
 import copy
 import os
 import sys
+import torch.nn.functional as F
+from torch.utils.data import dataloader
+from tqdm import tqdm
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class AdversialPointCloud():
-    def __init__(self, BATCH_SIZE, NUM_CLASSES,Th):
+    def __init__(self,Th):
         self.Th = Th
-    def GetSaliencyGradCam(self, model,pointCloud,label):
+    def GetSaliencyGradCam(self, classifier,dataloader,label):
         with tqdm(dataloader, unit="batch") as tepoch:
       
           for batchInd,data in enumerate(tepoch,0):
-              if(batchInd> 20):
-                continue
               batchSize = points.shape[0]  
               points, target = data
               target = target[:, 0]
@@ -29,21 +30,8 @@ class AdversialPointCloud():
                 pointCloud = points[indBatch,:].unsqueeze(0)
                 singleTarget = target.unsqueeze(0)
                 pred, trans, trans_feat = classifier(pointCloud)
-                
                 loss = F.nll_loss(pred, singleTarget)
-                
-
                 loss.backward()
-
                 gradientsPerPoint =torch.max(classifier.feat.gradients[0][0], 1, keepdim=False)[0]
                 #### getA is the sum of features where gradientsPerPoint are the re the gradients according to the last layer
-                getA = classifier(points, True)
-
-        """while pred == label:
-          derivatives = model.GetDevs(pointCloudCopy)
-          weights = model.GetWeights(pointCloudCopy,derivatives)
-          pointCloudCopy,placesWhereRemovedAndScores = removeNeededPoints(pointCloudCopy,weights,self.Th)
-          pred = np.argmax(model.forward(pointCloud))
-        return dictPlacesAndScores
-        """
-        
+                getA = classifier(points, True)     
