983fdc181f41384eda2b1c98893e05449e2152a2
IdoLev95 <idolev0611@gmail.com>
2022-05-06 07:40:24 +0000

    Adversial grad cam

--- Saliency/AdversialAttack.py
+++ Saliency/AdversialAttack.py
@@ -2,6 +2,7 @@ import argparse
 import numpy as np
 import torch
 import socket
+import argparse
 import importlib
 import copy
 import os
@@ -11,7 +12,17 @@ from torch.utils.data import dataloader
 from tqdm import tqdm
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--Th', type=int, default=1, help='input threshold')
+parser.add_argument(
+    '--label', type=int, default=1, help='input label')
+parser.add_argument(
+    '--model', help='classifier')
 
+parser.add_argument('--dataloader', help="dataloader")
+opt = parser.parse_args()
+print(opt)
 class AdversialPointCloud():
     def __init__(self,Th):
         self.Th = Th
@@ -19,8 +30,9 @@ class AdversialPointCloud():
         with tqdm(dataloader, unit="batch") as tepoch:
       
           for batchInd,data in enumerate(tepoch,0):
-              batchSize = points.shape[0]  
+              
               points, target = data
+              batchSize = points.shape[0]
               target = target[:, 0]
               points = points.transpose(2, 1)
               points, target = points.cuda(), target.cuda()
