77139ee318610cf80b761ea8c42327d9913931cc
IdoLev95 <idolev0611@gmail.com>
2022-05-20 18:10:50 +0000

    Refactor for working with classes

--- Training/ModelTrainer.py
+++ Training/ModelTrainer.py
@@ -1,36 +1,47 @@
+import torch
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torch.nn.functional as F
+from torch.utils.data import dataloader
+from tqdm import tqdm
+import sys
+sys.path.append('/content/UnsupervisedSaliencyPointCloud/Models')
+from model import PointNetCls, feature_transform_regularizer
 
 
 class ModelTrainer():
-  def __init__(self, train_data_loader, test_data_loader,classifier,optimizer,scheduler):
+  def __init__(self, train_data_loader, test_data_loader,classifier,optimizer,scheduler,batch_size):
         self.train_data_loader = train_data_loader
         self.test_data_loader = test_data_loader
+        self.batch_size = batch_size
         self.classifier = classifier
         self.optimizer = optimizer
         self.scheduler = scheduler
 
-  def train(num_epochs):
-    for epoch in range(opt.nepoch):
-      with tqdm(dataloader, unit="batch") as tepoch:
+  def train(self,num_epochs,feature_transform = False):
+    for epoch in range(num_epochs):
+      with tqdm(self.dataloader, unit="batch") as tepoch:
         lossOfEpoch = []
         accOfEpoch = []
-        scheduler.step()
+        self.scheduler.step()
         for batchInd,data in enumerate(tepoch,0):
           tepoch.set_description(f"Epoch {epoch}")
           points, target = data
           target = target[:, 0]
           points = points.transpose(2, 1)
           points, target = points.cuda(), target.cuda()
-          optimizer.zero_grad()
-          pred, trans, trans_feat = classifier(points)  
+          self.optimizer.zero_grad()
+          pred, trans, trans_feat = self.classifier(points)  
           loss = F.nll_loss(pred, target)
-          if opt.feature_transform:
+          if feature_transform:
               loss += feature_transform_regularizer(trans_feat) * 0.001
           loss.backward()
-          optimizer.step()
+          self.optimizer.step()
           pred_choice = pred.data.max(1)[1]
           correct = pred_choice.eq(target.data).cpu().sum()
           if batchInd % 10 == 0:
-              j, data = next(enumerate(testdataloader, 0))
+              j, data = next(enumerate(self.testdataloader, 0))
               points, target = data
               target = target[:, 0]
               points = points.transpose(2, 1)
@@ -41,11 +52,11 @@ class ModelTrainer():
               pred_choice = pred.data.max(1)[1]
               correct = pred_choice.eq(target.data).cpu().sum()
               lossOfEpoch.append(loss.item())
-              accOfEpoch.append(correct.item() / float(opt.batchSize))
+              accOfEpoch.append(correct.item() / float(self.batchSize))
               tepoch.set_postfix(loss=sum(lossOfEpoch)/len(lossOfEpoch), accuracy=100. * sum(accOfEpoch)/len(accOfEpoch))
           
 
-   torch.save(classifier.state_dict(), '/content/UnsupervisedSaliencyPointCloud/cls/cls_model_%d.pth' % (epoch))
+      torch.save(classifier.state_dict(), '/content/UnsupervisedSaliencyPointCloud/cls/cls_model_%d.pth' % (epoch))
 
         
 
